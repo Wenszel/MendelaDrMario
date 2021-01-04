a391e0a4129469bd0cbc6f825d3cd88e8c320830
Wenszel <wiktor.smaga13@gmail.com>
2021-01-04 14:52:37 +0100

    simple pill falling on arrowdown press

--- js/index.js
+++ js/index.js
@@ -12,8 +12,19 @@ var gameField = {
     elements: [], // two-dimensional array, stores grid objects
     startGame(){
         document.getElementById("main-page").style.display="none";
-        document.body.style.backgroundImage="url('gfx/game-pattern.png')"
-        this.initiate()
+        document.body.style.backgroundImage="url('gfx/game-pattern.png')";
+        this.initiate();
+        let newPill = new pill();
+        newPill.generatePill();
+        document.onkeydown = function(event){
+            const key = event.key;
+            switch(key){
+                case "ArrowDown": 
+                    newPill.fallOnce();
+                    break;
+            }
+            
+        };
     },
     initiate(){ // create game field and fill up elements array
         this.fieldDiv.id = "playground";
@@ -30,28 +41,56 @@ var gameField = {
                 this.fieldDiv.append(fieldElement.elementDiv);
                 rowArray.push(fieldElement);
             }
-            this.elements.push(rowArray)
+            this.elements.push(rowArray);
         }
-        document.body.appendChild(this.fieldDiv)
+        document.body.appendChild(this.fieldDiv);
+    },
+    changeElementColor(row, column, color){
+        this.elements[row][column].elementDiv.style.backgroundColor = color;
     }
 }
 class pill {
     constructor(){
-        this.row = 0
-        this.column = Math.floor(Math.random()*config.columns)
-        this.colors = pill.generateColors()
+        this.row = 0;
+        // this loop deletes case when column is out of range
+        do{
+            var randomColumn = Math.floor(Math.random()*config.columns); 
+        }while(randomColumn==config.columns-1);
+        this.column = [randomColumn, randomColumn+1];
+        this.colors = pill.generateColors();
     } 
     generatePill(){
+        gameField.changeElementColor(this.row, this.column[0], this.colors[0]);
+        gameField.changeElementColor(this.row, this.column[1], this.colors[1]);
     }
     fallOnce(){
+        if(this.isFallible(this.row, this.column[0])){
+            gameField.changeElementColor(this.row, this.column[0], "#282828");
+            gameField.changeElementColor(this.row, this.column[1], "#282828");
+            this.row++;
+            gameField.changeElementColor(this.row, this.column[0], this.colors[0]);
+            gameField.changeElementColor(this.row, this.column[1], this.colors[1]);
+        }
     }
-    isFallible(){
+    isFallible(row, column){
+        //check if fields below pill are empty and in rows scope
+        try{
+            if(gameField.elements[row+1][column].empty){
+                return true;
+            }else{
+                return false;
+            }
+        }catch (error){
+            return false;
+        }
     }
     static generateColors(){
-        var colors = []
-        for(var i = 0; i<2; i++){
-            colors.push(config.colors[Math.floor(Math.random()*config.colors.length)])
-        }
+        do{
+            var colors = [];
+            for(let i = 0; i<2; i++){
+                colors.push(config.colors[Math.floor(Math.random()*config.colors.length)]);
+            }
+        }while(colors[0]==colors[1])
         return colors;
     }
 };
